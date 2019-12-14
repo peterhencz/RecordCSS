2bffa13a80584d327b0a41b9fc37f7e26b90f07a
Peter Hencz <peter.hencz@gmail.com>
2019-12-14 01:49:45 +0100

    test

--- script.js
+++ script.js
@@ -3,25 +3,26 @@ const tiles = [
     artist: "Mars Volta",
     album: "Noctourniquet",
     bgColor: "#dedede",
-    textColor: "#9a9692",
+    textColor: "#14375A",
   },
   {
     artist: "Mount Kimbie",
     album: "Cold Spring Fault Less Youth",
-    bgColor: "#3648d0",
-    textColor: "#9a9692",
+
+    bgColor: "#99B239",
+    textColor: "#F6F6F6",
   },
   {
     artist: "Jon Hopkins",
     album: "Remixes",
-    bgColor: "#bdc6c9",
-    textColor: "#9a9692",
+    bgColor: "#3648d0",
+    textColor: "#D4D6D9",
   },
   {
     artist: "Four Tet",
     album: "Beautiful Rewind",
-    bgColor: "#acbda8",
-    textColor: "#9a9692",
+    bgColor: "#bdc6c9",
+    textColor: "#B23939",
   },
 ];
 
@@ -29,18 +30,21 @@ const createCards = () => {
   console.table(tiles);
   Object.values(tiles).forEach(function(item) {
     const main = document.querySelector(".main-container");
+    const tileContainer = document.createElement("div");
     const tile = document.createElement("div");
     const coverArt = document.createElement("div");
     const coverText = document.createElement("div");
     const tileH1 = document.createElement("h1");
     const tileH2 = document.createElement("h2");
     const close = document.createElement("div");
+    let depth = 0;
 
+    tileContainer.classList.add("tile-container");
     tile.classList.add("cover-tile");
     tile.style.backgroundColor = item.bgColor;
 
     coverArt.classList.add("cover-art");
-    coverArt.style.backgroundColor = "tomato";
+    coverArt.style.backgroundColor = "#262C2E";
     coverText.classList.add("cover-text");
 
     tileH1.innerHTML = item.artist;
@@ -53,33 +57,25 @@ const createCards = () => {
     coverText.appendChild(tileH2);
     tile.appendChild(coverArt);
     tile.appendChild(coverText);
-    main.appendChild(tile);
+    tileContainer.appendChild(tile);
+    main.appendChild(tileContainer);
 
     coverArt.addEventListener("click", () => {
       tile.classList.add("tile-full-size");
-      tile.classList.remove("cover-tile");
+      tile.style.zIndex = depth;
+      depth = depth + 1;
       close.classList.add("cover-close");
 
       close.innerHTML = "Close";
       close.style.display = "block";
       close.style.color = item.textColor;
       coverArt.style.height = coverArt.style.width;
-      const tilesToHide = document.querySelectorAll(".cover-tile");
-
-      tilesToHide.forEach(element => {
-        element.style.display = "none";
-      });
-
+      console.log(depth);
       tile.appendChild(close);
     });
 
     close.addEventListener("click", () => {
-      const tilesToHide = document.querySelectorAll(".cover-tile");
-      tilesToHide.forEach(element => {
-        element.style.display = "flex";
-      });
       tile.classList.remove("tile-full-size");
-      tile.classList.add("cover-tile");
       close.style.display = "none";
     });
   });
